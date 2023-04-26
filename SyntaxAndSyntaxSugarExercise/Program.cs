b3ea6a425b381ef499f6f5857687104b148a7f5f
Bailey Hart <baileyhart100@gmail.com>
2023-04-26 15:55:46 -0400

    initial commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //string response;//Explicit Typing
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "greater than or equal to nine";
+            //}
+            //Console.WriteLine(response);
+
+            //Inferred Typing
+            var answer = 4;
+            var response = (answer < 9)? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
         }
     }
 }
